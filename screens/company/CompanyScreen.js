f96357f6343f00231601b097c570ad752bfe4bba
Айдын Ибраев (Aydyn Ibraev) <AIbraev@alfabank.kz>
2020-05-17 15:18:05 +0600

    feat: bottom shet view

--- screens/company/CompanyScreen.js
+++ screens/company/CompanyScreen.js
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   StyleSheet,
   View,
   ActivityIndicator,
   Image,
-  Button,
   TouchableOpacity,
   Text,
 } from 'react-native';
 import { useQuery } from '@apollo/react-hooks';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import RBSheet from 'react-native-raw-bottom-sheet';
 import CompanyInfo from './components/company-info';
 import CompanyServices from './components/company-service-list';
-import { width } from '../../constants/Layout';
+import { width, height } from '../../constants/Layout';
 import { GET_COMPANY_SERVICES } from '../../queries/company';
-
-const initialLayout = { width };
 
 const renderTabBar = (props) => (
   <TabBar
@@ -25,7 +23,15 @@ const renderTabBar = (props) => (
   />
 );
 
+const routes = [
+  { key: 'first', title: 'О компании' },
+  { key: 'second', title: 'Услуги' },
+];
+
 export default function CompanyScreen({ route, navigation }) {
+  const [index, setIndex] = useState(0);
+  const [sheetView, setSheetView] = useState(false);
+  const refRBSheet = useRef();
   const { businessCompanyID, businessCompanyName } = route.params;
   navigation.setOptions({
     headerTitle: businessCompanyName,
@@ -35,11 +41,6 @@ export default function CompanyScreen({ route, navigation }) {
       fontWeight: 'bold',
     },
   });
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: 'first', title: 'О компании' },
-    { key: 'second', title: 'Услуги' },
-  ]);
 
   const renderScene = SceneMap({
     first: () => <CompanyInfo data={route.params} />,
@@ -63,7 +64,12 @@ export default function CompanyScreen({ route, navigation }) {
   }
 
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        sheetView && { opacity: 0.5, backgroundColor: '#000' },
+      ]}
+    >
       <Image
         style={styles.company_image}
         source={require('../../assets/images/company.jpg')}
@@ -72,14 +78,33 @@ export default function CompanyScreen({ route, navigation }) {
         navigationState={{ index, routes }}
         renderScene={renderScene}
         onIndexChange={setIndex}
-        initialLayout={initialLayout}
+        initialLayout={{ width }}
         renderTabBar={renderTabBar}
       />
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          refRBSheet.current.open();
+          setSheetView(true);
+        }}
+      >
         <View style={styles.button}>
           <Text style={styles.button_text}>Записаться</Text>
         </View>
       </TouchableOpacity>
+      <RBSheet
+        ref={refRBSheet}
+        onClose={() => setSheetView(false)}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        height={height * 0.5}
+        customStyles={{
+          draggableIcon: {
+            backgroundColor: '#000',
+          },
+        }}
+      >
+        <Text>sheet view</Text>
+      </RBSheet>
     </View>
   );
 }
